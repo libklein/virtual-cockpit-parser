2f158517203f9aca752c9a1eb644ec1acc30434a
Patrick Sean Klein <patrick@libklein.com>
2025-07-05 21:04:25 +0100

    Start implementing simple handwritten lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,128 @@
 pub mod parser {}
 
 pub mod lexer {
-    use std::{fs::read_to_string, path::Path};
+    use core::fmt;
+    use std::{
+        error::Error,
+        fs::read_to_string,
+        path::{Path, PathBuf},
+    };
 
-    use logos::{Lexer, Logos, Skip};
-
-    #[derive(Logos, Debug, PartialEq)]
-    #[logos(skip r"[ \t\r\n\f]+", extras = &'s Path)]
+    #[derive(Debug, PartialEq)]
     pub enum Token {
-        #[regex(r"#Include\([\./a-zA-Z0-9_\-\s\&\+]+\)#", parse_include)]
-        Include(Vec<Token>),
-
-        #[token("}")]
+        Text(String),
+        Equals,
+        Semicolon,
+        BlockBegin,
         BlockEnd,
-
-        #[regex(r"\w[\w\-\s]*=[^;\}\{]*;?", parse_assignment)]
-        Assignment(Vec<String>),
-
-        #[regex(r"([^\s\{\}\/][\S&&[^\{]]*)?\{", parse_block_begin)]
-        BlockBegin(Option<String>),
-
-        #[regex(r"//.*\n", |_| Skip)]
-        Comment,
     }
 
-    fn parse_block_begin(lexer: &mut Lexer<Token>) -> Option<String> {
-        let slice = lexer.slice();
-        if slice.ends_with('{') {
-            Some(slice[..slice.len() - 1].trim().to_string())
-        } else {
+    #[derive(Debug)]
+    pub struct LexerError {
+        path: PathBuf,
+        partial_token: String,
+    }
+
+    impl fmt::Display for LexerError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Error parsing file {}.", self.path.display())
+        }
+    }
+    impl Error for LexerError {}
+
+    pub struct Lexer<'a> {
+        text: String,
+        file_path: &'a Path,
+        buf_start: usize,
+    }
+
+    impl<'a> Lexer<'a> {
+        pub fn new(text: String, file_path: &'a Path) -> Self {
+            Lexer {
+                text,
+                file_path,
+                buf_start: 0,
+            }
+        }
+
+        pub fn from_file(file_path: &'a Path) -> Self {
+            let file_content = read_to_string(file_path).unwrap();
+            Lexer {
+                text: file_content,
+                file_path,
+                buf_start: 0,
+            }
+        }
+    }
+
+    impl<'a> Iterator for Lexer<'a> {
+        type Item = Result<Token, LexerError>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let mut buf_len = 0;
+            let mut non_whitespace_len = 0;
+
+            let remaining_text = &self.text[self.buf_start..];
+            for next_char in remaining_text.chars() {
+                let num_bytes = next_char.len_utf8();
+                // Ignore whitespace
+                if next_char.is_whitespace() {
+                    buf_len += num_bytes;
+                    continue;
+                }
+
+                // TODO: Handle comments
+
+                match next_char {
+                    '=' | '{' | '}' | '#' | ';' => {
+                        if non_whitespace_len > 0 {
+                            self.buf_start += buf_len;
+                            // Text
+                            return Some(Ok(Token::Text(
+                                remaining_text[..buf_len].trim().to_owned(),
+                            )));
+                        } else {
+                            // Handle special character
+                            self.buf_start += buf_len + num_bytes;
+                            return match next_char {
+                                '=' => Some(Ok(Token::Equals)),
+                                '{' => Some(Ok(Token::BlockBegin)),
+                                '}' => Some(Ok(Token::BlockEnd)),
+                                ';' => Some(Ok(Token::Semicolon)),
+                                '#' => todo!("Include found!"),
+                                _ => panic!("Unhandled special character {}", next_char),
+                            };
+                        }
+                    }
+                    _ => {}
+                }
+
+                buf_len += num_bytes;
+                non_whitespace_len += num_bytes;
+            }
+
+            // We've still had input left
+            if non_whitespace_len > 0 {
+                return Some(Err(LexerError {
+                    path: self.file_path.to_owned(),
+                    partial_token: remaining_text[0..buf_len].to_owned(),
+                }));
+            }
             None
         }
     }
 
-    fn parse_assignment(lexer: &mut Lexer<Token>) -> Option<Vec<String>> {
-        Some(
-            lexer
-                .slice()
-                .strip_suffix(";")
-                .unwrap_or(lexer.slice())
-                .split('=')
-                .map(|s| s.trim().to_string())
-                .collect(),
-        )
-    }
-
-    fn parse_include(lexer: &mut Lexer<Token>) -> Option<Vec<Token>> {
-        println!("Parsing include directive: {}", lexer.slice());
-        let included_file_path = &(lexer.slice()[9..lexer.slice().len() - 2]);
-        let resolved_path = lexer.extras.join(included_file_path);
-        println!("Parsing path {}", resolved_path.display());
-        lex(lexer.extras, &read_to_string(resolved_path).unwrap())
-    }
-
-    pub fn lex(file_path: &Path, text: &str) -> Option<Vec<Token>> {
-        let mut lex = Token::lexer_with_extras(text, file_path);
-        let mut vec = vec![];
-        while let Some(parsed_token) = lex.next() {
-            if let Ok(token) = parsed_token {
-                println!("TOKEN: {:?}", token);
-                vec.push(token);
+    pub fn lex(file_path: &Path, text: &str) -> Result<Vec<Token>, LexerError> {
+        let lexer = Lexer::new(text.to_owned(), file_path);
+        for next_token in lexer {
+            if let Ok(next_token) = next_token {
+                println!("{:?}", next_token)
             } else {
-                println!("ERROR: {:?}", lex.slice());
-                return None;
+                println!("Error");
+                break;
             }
         }
-        Some(vec)
+        Ok(vec![])
     }
 
     #[cfg(test)]
@@ -85,7 +142,7 @@ pub mod lexer {
                 }
                 println!("Cockpit file: {}", cockpit_file.to_string_lossy());
 
-                assert!(lex(&dir.path(), &read_to_string(cockpit_file).unwrap()).is_some());
+                assert!(lex(&dir.path(), &read_to_string(cockpit_file).unwrap()).is_ok());
             }
         }
     }
